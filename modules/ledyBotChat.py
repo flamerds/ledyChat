30ced04505fc2e0336457a37c7e9f7132448230d
popcorn9499 <popcorn9499@gmail.com>
2018-12-15 08:49:03 -0600

    cleaned up the code

--- modules/ledyBotChat.py
+++ modules/ledyBotChat.py
@@ -14,26 +14,10 @@ class ledyBotChat:
         self.l.logger.info("Starting")
         loop = asyncio.get_event_loop()
         loop.create_task(self.ledyCommands())
-        #config.events.onMessage += self.startLedyBot
-        #config.events.onMessage += self.stopLedyBot
-        #config.events.onMessage += self.connectDSLedyBot
-        #config.events.onMessage += self.disconnectDSLedyBot
-        self.l.logger.info("Starting")
-        self.l.logger.info("Starting")
-        self.l.logger.info("Starting")
-        self.l.logger.info("Starting")
-        self.l.logger.info("Starting")
-        self.l.logger.info("Starting")
-        self.l.logger.info("Starting")
-        self.l.logger.info("Starting")
-        self.l.logger.info("Starting")
-        
-
         self.l.logger.info("Started")
         self.ledyPipeObj = pipeClient.pipeClient(r"\\.\pipe\Demo")
         self.ledyPipeReaderObj = pipeClient.pipeClient(r"\\.\pipe\Demo1")
         loop.create_task(self.ledyReader())
-        #self.ledyPipeObj.start()
 
     async def ledyReader(self):
         while True:
@@ -59,18 +43,13 @@ class ledyBotChat:
         config.events.addCommandType(commandType="ledyDsRefresh",commandHandler=self.refreshLedyBot)
         config.events.addCommandType(commandType="ledyDsTradequeue",commandHandler=self.tradequeueLedyBot)
         config.events.addCommandType(commandType="ledyDsViewqueue",commandHandler=self.viewqueueLedyBot)
-        #config.events.addCommandType(commandType="ledyDsStart",commandHandler=self.startLedyBot)
 
 
 
     async def startLedyBot(self,message,command):
-        # if (message.Message.Contents == "!START") & await self.roleChecker(["Normal"],message.Message.Roles) == True:
-        #     self.l.logger.info("Starting Ledybot")
         await self.ledyPipeObj.pipeWriter("startgtsbot")
 
     async def stopLedyBot(self,message,command):
-        # if (message.Message.Contents == "!STOP") & await self.roleChecker(["Normal"],message.Message.Roles) == True:
-        #     self.l.logger.info("Starting Ledybot")
         await self.ledyPipeObj.pipeWriter("stopgtsbot")
         commandOutput = await self.ledyPipeObj.pipeReader()
         commandOutput = await self.messageFix(commandOutput)
@@ -79,8 +58,6 @@ class ledyBotChat:
 
             
     async def connectDSLedyBot(self,message,command):
-        # if (message.Message.Contents == "!CONNECT") & await self.roleChecker(["Normal"],message.Message.Roles) == True:
-        #     self.l.logger.info("Starting Ledybot")
         await self.ledyPipeObj.pipeWriter("connect3ds")
         commandOutput = await self.ledyPipeObj.pipeReader()
         commandOutput = await self.messageFix(commandOutput)
@@ -89,8 +66,6 @@ class ledyBotChat:
   
 
     async def disconnectDSLedyBot(self,message,command):
-        # if (message.Message.Contents == "!DISCONNECT") & await self.roleChecker(["Normal"],message.Message.Roles) == True:
-        #     self.l.logger.info("Starting Ledybot")
         await self.ledyPipeObj.pipeWriter("disconnect3ds")   
         commandOutput = await self.ledyPipeObj.pipeReader()
         commandOutput = await self.messageFix(commandOutput)
@@ -99,8 +74,6 @@ class ledyBotChat:
   
 
     async def refreshLedyBot(self,message,command):
-        # if (message.Message.Contents == "!DISCONNECT") & await self.roleChecker(["Normal"],message.Message.Roles) == True:
-        #     self.l.logger.info("Starting Ledybot")
         splitMsg = message.Message.Contents.split(" ")
         if len(splitMsg) == 3:
             await self.ledyPipeObj.pipeWriter("refresh {0} {1}".format(splitMsg[1],splitMsg[2])) 
@@ -115,8 +88,6 @@ class ledyBotChat:
     
 
     async def tradequeueLedyBot(self,message,command):
-        # if (message.Message.Contents == "!DISCONNECT") & await self.roleChecker(["Normal"],message.Message.Roles) == True:
-        #     self.l.logger.info("Starting Ledybot")
         await self.ledyPipeObj.pipeWriter("togglequeue")
         commandOutput = await self.ledyPipeObj.pipeReader()  
         commandOutput = await self.messageFix(commandOutput)
@@ -124,8 +95,6 @@ class ledyBotChat:
         await self.processMsg(message=commandOutput,username="Bot",channel=message.Message.Channel,server=message.Message.Server,service=message.Message.Service,roleList=botRoles)       
 
     async def viewqueueLedyBot(self,message,command):
-        # if (message.Message.Contents == "!DISCONNECT") & await self.roleChecker(["Normal"],message.Message.Roles) == True:
-        #     self.l.logger.info("Starting Ledybot")
         await self.ledyPipeObj.pipeWriter("viewqueue")
         commandOutput = await self.ledyPipeObj.pipeReader()  
         commandOutput = await self.messageFix(commandOutput)
